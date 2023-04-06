046c012124a20c1a6dbb7c3ccd0c6913c132e0fc
Gabriel - hckr <gabu.1991@gmail.com>
2023-04-05 20:02:59 -0400

    arregle el ejercicio de adivinanza en un solo while

--- PR1/Unidad_1/src/Unidad_5/Tarea_1/Adivinanza.java
+++ PR1/Unidad_1/src/Unidad_5/Tarea_1/Adivinanza.java
@@ -6,32 +6,25 @@ import java.util.Scanner;
 public class Adivinanza {
     public static void main(String[] args) {
         Random numRandom = new Random();
-        int num;
+        int num = 1;
+        int exit = 0;
         Scanner sc = new Scanner(System.in);
-        boolean band = false;
-        boolean band2 = false;
+        int adivinar = 1 + numRandom.nextInt(20);
 
-        while(!band){
-            int adivinar = 1 + numRandom.nextInt(20);
-            while(!band2){
-                System.out.print("Adivine el numero entre 1 y 20: ");
-                num = sc.nextInt();
-                if(num < adivinar)
-                    System.out.println("Demasiado bajo. Intente de nuevo");
-
-                if(num > adivinar)
-                    System.out.println("Demasiado alto. Intente de nuevo");
-
-                if(num == adivinar){
-                    System.out.println("Felicidades, adivino el numero!");
-                    band2 = true;
-                }
-            }
-            band2 = false;
-            System.out.println("Desea jugar otra vez? 1 = SI / 2 = NO");
+        while(exit != 2){
+            System.out.print("Adivine el numero entre 1 y 20: ");
             num = sc.nextInt();
-            if(num == 2) {
-                band = true;
+            if(num < adivinar)
+                System.out.println("Demasiado bajo. Intente de nuevo");
+
+            if(num > adivinar)
+                System.out.println("Demasiado alto. Intente de nuevo");
+
+            if(num == adivinar){
+                System.out.println("Felicidades, adivino el numero!");
+                System.out.println("Desea jugar otra vez? 1 = SI / 2 = NO");
+                exit = sc.nextInt();
+                adivinar = 1 + numRandom.nextInt(20);
             }
         }
     }
